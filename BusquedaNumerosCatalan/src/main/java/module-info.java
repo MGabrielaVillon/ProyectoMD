05e19b07cba3c03101def009ab56680d43cec948
MGabrielaVillon <117649344+MGabrielaVillon@users.noreply.github.com>
2023-09-04 03:31:54 -0500

    Delete BusquedaNumerosCatalan directory

--- BusquedaNumerosCatalan/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.espol.searchernumeroscatalan {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.espol.searchernumeroscatalan to javafx.fxml;
-    exports com.espol.searchernumeroscatalan;
-}
